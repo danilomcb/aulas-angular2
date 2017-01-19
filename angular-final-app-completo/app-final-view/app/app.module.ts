c7fde3eee5859ba5419ce1778fb0e118c762ac2d
Danilo Barros <danilocosta14@gmail.com>
2017-01-19 18:10:26 -0300

    Criada estrutura inicial do projeto

---
+++ angular-final-app-completo/app-final-view/app/app.module.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by danilo-barros on 19/01/17.
+ */
+
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {AppComponent} from "./app.component";
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ angular-final-app-completo/app-final-view/app/main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by danilo-barros on 19/01/17.
+ */
+
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import {AppModule} from "./app.module";
+
+platformBrowserDynamic().bootstrapModule(AppModule);
